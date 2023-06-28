2e053a863240aebd904797dc30b436012696af36
Aleksandr <aleksandr-s86>
2023-06-28 17:23:02 +0300

    сумма и факториал числа

---
+++ Задача1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Задача1 {
+
+    public static void main(String[] args) {
+       num();
+    }
+    static void num(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input yuor num: ");
+        int num =sc.nextInt();
+        int sum = 0;
+        int fact =1;
+        for (int index = 0; index < num; index++) {
+            sum+=index+1;    
+        }
+        for (int index = 0; index < num; index++) {
+            fact *=index+1;
+        }
+        System.out.printf("Factorial num =%d",fact);
+        System.out.printf("\nSumma at1 to num = %d",sum);
+        sc.close();
+    }
+}
